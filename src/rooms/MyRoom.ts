67ae96b3111b935f3ce3994edd50a7dfa67dca84
WDDA <dima@wdda.pro>
2023-02-28 19:59:06 +0300

    Quest

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -26,7 +26,6 @@ export class MyRoom extends Room<MyRoomState> {
       } else {
         axios.get('https://promo.battlemon.com/api/lemon/' + message.playerId)
           .then(result => {
-            console.log(result.data)
             this.pushData(result.data, client.sessionId)
           }).catch((error) => {
           if (error.response) {
