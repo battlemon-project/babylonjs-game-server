3d84261789a6ef435e8505d4246ab6683357afa6
WDDA <dima@wdda.pro>
2023-02-28 19:26:33 +0300

    Quest

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -1,6 +1,5 @@
 import { Room, Client } from "colyseus"
 import { Item, MyRoomState, Player, Property } from './schema/MyRoomState'
-import { ArraySchema } from "@colyseus/schema";
 import axios from 'axios'
 
 export class MyRoom extends Room<MyRoomState> {
@@ -8,32 +7,33 @@ export class MyRoom extends Room<MyRoomState> {
     this.setState(new MyRoomState());
     
     this.onMessage('createPlayer', (client, message) => {
-      axios.get('https://promo.battlemon.com/api/lemon/' + message.playerId)
-        .then(result => {
-          const data = result.data
-          
-          let player: Player = new Player()
-          player.id = data.id
-          player.clientSessionId = client.sessionId
-          player.items = []
-          player.properties = []
-  
-          data.items.forEach((item: any) => {
-            let newItem = new Item()
-            newItem.type = item.type
-            newItem.flavour = item.flavour
-            player.items.push(newItem)
-          })
-  
-          data.properties.forEach((property: any) => {
-            let newProperty = new Property()
-            newProperty.type = property.type
-            newProperty.flavour = property.flavour
-            player.properties.push(newProperty)
-          })
-          
-          this.state.players.push(player)
+      if (message.playerId.includes('guest_')) {
+        const data: any = {
+          id: message.playerId,
+          items: [],
+          properties: [
+            { type: 'exo_top', flavour: 'ExoTop_Snowwhite' },
+            { type: 'exo_bot', flavour: 'ExoBot_Snowwhite' },
+            { type: 'feet', flavour: 'Feet_Snowwhite' },
+            { type: 'eyes', flavour: 'Eyes_Blue' },
+            { type: 'hands', flavour: 'Hands_Snowwhite' },
+            { type: 'head', flavour: 'Head_Fresh_Lemon' },
+            { type: 'teeth', flavour: 'Teeth_Hollywood' }
+          ],
+        }
+        
+        this.pushData(data, client.sessionId)
+      } else {
+        axios.get('https://promo.battlemon.com/api/lemon/' + message.playerId)
+          .then(result => {
+            this.pushData(result.data, client.sessionId)
+          }).catch((error) => {
+          if (error.response) {
+            client.send('errorIdPlayer', error.response.status)
+          }
         })
+        
+      }
     })
   
     this.onMessage("syncPlayer", (client, message) => {
@@ -54,5 +54,29 @@ export class MyRoom extends Room<MyRoomState> {
 
   onDispose() {
     console.log("room", this.roomId, "disposing...");
+  }
+  
+  pushData(data: any, sessionId: string) {
+    let player: Player = new Player()
+    player.id = data.id
+    player.clientSessionId = sessionId
+    player.items = []
+    player.properties = []
+  
+    data.items.forEach((item: any) => {
+      let newItem = new Item()
+      newItem.type = item.type
+      newItem.flavour = item.flavour
+      player.items.push(newItem)
+    })
+  
+    data.properties.forEach((property: any) => {
+      let newProperty = new Property()
+      newProperty.type = property.type
+      newProperty.flavour = property.flavour
+      player.properties.push(newProperty)
+    })
+  
+    this.state.players.push(player)
   }
 }
